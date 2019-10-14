c8d4258ed00423b884a4f85e3d4fc9de843c31a0
davidsherrick <dsherrick@synthesissolutions.com>
2019-10-14 17:26:10 -0500

    test code for trying different pull speeds with the foundation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TELE/PlatformPullTest.java
@@ -0,0 +1,243 @@
+package org.firstinspires.ftc.teamcode.TELE;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import static com.qualcomm.robotcore.util.Range.scale;
+
+@TeleOp(name="Platform Pull Testing", group="TELE")
+public class PlatformPullTest extends OpMode {
+
+    final static double MECANUM_MAX_SPEED = 1.0;
+    final static double MECANUM_HIGH_SPEED = 0.8;
+    final static double MECANUM_MEDIUM_SPEED = 0.6;
+    final static double MECANUM_LOW_SPEED = 0.4;
+
+    final static double SLOW_STRAFE_FACTOR = 1.4;
+    final static double SLOW_TURN_FACTOR = 1.20;
+    final static double SLOW_SPEED_FACTOR = 1.4;
+
+    final static double FOUNDATION_GRABBER_LEFT_UP = 0.18;
+    final static double FOUNDATION_GRABBER_LEFT_DOWN = 0.91;
+    final static double FOUNDATION_GRABBER_RIGHT_UP = 1.0;
+    final static double FOUNDATION_GRABBER_RIGHT_DOWN = 0.23;
+
+    DcMotor motorFrontLeft;
+    DcMotor motorFrontRight;
+    DcMotor motorBackRight;
+    DcMotor motorBackLeft;
+
+    Servo foundationGrabberLeft;
+    Servo foundationGrabberRight;
+
+    double currentMaxSpeed = MECANUM_MEDIUM_SPEED;
+
+    double currentPullSpeed = 0.25;
+    double pullSpeedStepSize = 0.05;
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private ElapsedTime buttonTime = new ElapsedTime();
+    double pullTimeSeconds = 2.0;
+
+    @Override
+    public void init() {
+        buttonTime.reset();
+        initializeMecanum();
+        //initializeFoundationGrabbers();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Pull Speed", "" + currentPullSpeed);    //
+        telemetry.update();
+
+        double mecanumSpeed = -gamepad1.left_stick_y;
+        double mecanumTurn = gamepad1.right_stick_x;
+        double mecanumStrafe = -gamepad1.left_stick_x;
+
+        boolean mecanumSlowStrafe = gamepad1.left_trigger>.7;
+        boolean mecanumSlowSpeed = gamepad1.left_trigger>.7;
+        boolean mecanumSlowTurn = gamepad1.right_trigger>.7;
+
+        if (buttonTime.seconds() > 0.5) {
+            if (gamepad1.dpad_up) {
+                currentPullSpeed += pullSpeedStepSize;
+                buttonTime.reset();
+            } else if (gamepad1.dpad_down) {
+                currentPullSpeed -= pullSpeedStepSize;
+                buttonTime.reset();
+            }
+        }
+
+        if (currentPullSpeed > 1.0) {
+            currentPullSpeed = 1.0;
+        } else if (currentPullSpeed <= 0) {
+            currentPullSpeed = 0.05;
+        }
+
+        if (gamepad1.a) {
+            motorFrontRight.setPower(currentPullSpeed);
+            motorFrontLeft.setPower(currentPullSpeed);
+            motorBackRight.setPower(currentPullSpeed);
+            motorBackLeft.setPower(currentPullSpeed);
+
+            runtime.reset();
+            while (runtime.seconds() < pullTimeSeconds) {
+                // nothing to do
+            }
+
+            motorFrontRight.setPower(0);
+            motorFrontLeft.setPower(0);
+            motorBackRight.setPower(0);
+            motorBackLeft.setPower(0);
+        }
+
+        //MAIN DRIVE
+        controlMecanumWheels(mecanumSpeed,mecanumTurn,mecanumStrafe,mecanumSlowStrafe,mecanumSlowSpeed,mecanumSlowTurn);
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    public void initializeMecanum()
+    {
+        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+
+        motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motorBackRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    public void initializeFoundationGrabbers() {
+        foundationGrabberLeft = hardwareMap.servo.get("servoFoundationGrabberLeft");
+        foundationGrabberRight = hardwareMap.servo.get("servoFoundationGrabberRight");
+        raiseFoundationGrabber();
+    }
+
+    public void raiseFoundationGrabber () {
+        foundationGrabberLeft.setPosition(FOUNDATION_GRABBER_LEFT_UP);
+        foundationGrabberRight.setPosition(FOUNDATION_GRABBER_RIGHT_UP);
+    }
+
+    public void lowerFoundationGrabbers () {
+        foundationGrabberLeft.setPosition(FOUNDATION_GRABBER_LEFT_DOWN);
+        foundationGrabberRight.setPosition(FOUNDATION_GRABBER_RIGHT_DOWN);
+    }
+
+    public void controlMecanumWheels(double sp,double tu, double st, boolean slowSt, boolean slowSp, boolean slowTu)
+    {
+        double speed = sp;
+        double turn = tu;
+        double strafe = st;
+
+        strafe = slowStrafe(slowSt, strafe);
+
+        speed = slowSpeed(slowSp, speed);
+
+        turn = slowTurn(slowTu, turn);
+
+        holonomic(speed, turn, strafe, currentMaxSpeed);
+    }
+
+    public double slowSpeed(boolean active, double speed)
+    {
+        if (active) {
+            speed=speed/ SLOW_SPEED_FACTOR;
+            speed=speed/ SLOW_SPEED_FACTOR;
+        }
+        return speed;
+    }
+
+    public double slowStrafe(boolean active, double strafe)
+    {
+        if (active) {
+            strafe=strafe/ SLOW_STRAFE_FACTOR;
+        }
+        return strafe;
+    }
+
+    public double slowTurn(boolean active, double turn)
+    {
+        if (active)
+        {
+            turn=turn/ SLOW_TURN_FACTOR;
+        }
+        return turn;
+    }
+
+    public void holonomic(double mecanumSpeed, double mecanumTurn, double mecanumStrafe, double mecanumMaxSpeed)
+    {
+        //Left Front = +mecanumSpeed + mecanumTurn - mecanumStrafe      Right Front = +mecanumSpeed - mecanumTurn + mecanumStrafe
+        //Left Rear  = +mecanumSpeed + mecanumTurn + mecanumStrafe      Right Rear  = +mecanumSpeed - mecanumTurn - mecanumStrafe
+
+        double mecanumMagnitude = Math.abs(mecanumSpeed) + Math.abs(mecanumTurn) + Math.abs(mecanumStrafe);
+        mecanumMagnitude = (mecanumMagnitude > 1) ? mecanumMagnitude : 1; //Set scaling to keep -1,+1 range
+        final double MECANNUM_BACK_WHEEL_MULTIPLIER = 0.95;
+
+        if (motorFrontLeft != null)
+        {
+            motorFrontLeft.setPower(-scale((scaleInput(mecanumSpeed) + scaleInput(mecanumTurn) - scaleInput(mecanumStrafe)),
+                    -mecanumMagnitude, +mecanumMagnitude, -mecanumMaxSpeed, +mecanumMaxSpeed));
+        }
+        if (motorBackLeft != null)
+        {
+            motorBackLeft.setPower(-scale((scaleInput(mecanumSpeed) + scaleInput(mecanumTurn) + scaleInput(mecanumStrafe* MECANNUM_BACK_WHEEL_MULTIPLIER)),
+                    -mecanumMagnitude, +mecanumMagnitude, -mecanumMaxSpeed, +mecanumMaxSpeed));
+        }
+        if (motorFrontRight != null) {
+            motorFrontRight.setPower(-(scale((scaleInput(mecanumSpeed) - scaleInput(mecanumTurn) + scaleInput(mecanumStrafe)),
+                    -mecanumMagnitude, +mecanumMagnitude, -mecanumMaxSpeed, +mecanumMaxSpeed)));
+        }
+        if (motorBackRight != null) {
+            motorBackRight.setPower(-(scale((scaleInput(mecanumSpeed) - scaleInput(mecanumTurn) - scaleInput(mecanumStrafe* MECANNUM_BACK_WHEEL_MULTIPLIER)),
+                    -mecanumMagnitude, +mecanumMagnitude, -mecanumMaxSpeed, +mecanumMaxSpeed)));
+        }
+    }
+
+    double scaleInput(double dVal)
+    {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+}
