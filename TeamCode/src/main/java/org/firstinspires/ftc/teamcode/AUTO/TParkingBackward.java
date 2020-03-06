3dab19969aab34db061d700ba11f501c828aefe2
David Sherrick <dsherrick@synthesissolutions.com>
2020-03-05 23:31:40 -0600

    adding parking auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTO/TParkingBackward.java
@@ -0,0 +1,36 @@
+
+package org.firstinspires.ftc.teamcode.AUTO;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "TParkingBackward", group = "Linear Opmode")
+@Disabled
+public class TParkingBackward extends LinearOpMode {
+
+    AutoTabasco tabasco = new AutoTabasco();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        tabasco.initializeRobot(hardwareMap);
+        tabasco.angles = tabasco.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        telemetry.addData("currentAngle", tabasco.angles.firstAngle);
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        tabasco.prepRobot();
+        //delay(2.0);
+
+        tabasco.driveStraightBack(0.3, 500);
+
+        tabasco.shutdownRobot();
+    }
+
+}
